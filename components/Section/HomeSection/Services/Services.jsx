a77f88379d21f579dfff91d9e984d596d9568cae
Tarek Aziz <mdaziz3289@gmail.com>
2023-08-07 23:38:15 +0600

    first cm

--- components/Section/HomeSection/Services/Services.jsx
+++ components/Section/HomeSection/Services/Services.jsx
@@ -1,9 +1,18 @@
 
 const Services = () => {
     return (
-        <div className="min-h-screen mx-auto container flex items-center justify-center">
-            <p className="text-5xl font-bold">All Services Here </p>
+      <div className="min-h-screen mx-auto container flex items-center justify-center">
+        <div>
+          <p className="text-5xl font-bold">All Services Here </p>
+
+          <ul className="secondary-text mt-10">
+            <li className="text-xl">Oil Changed</li>
+            <li className="text-xl">Tires Serviced</li>
+            <li className="text-xl">Exchange Air Filter</li>
+            <li className="text-xl">Replace Ancient Coolant</li>
+          </ul>
         </div>
+      </div>
     );
 };
 
