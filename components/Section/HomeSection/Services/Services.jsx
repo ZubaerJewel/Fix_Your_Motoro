1f8d616f6540d9639f8bc67470e801014e7c1c12
trtonmoy <tr.tonmoy0110.trt@gmal.com>
2023-08-07 20:08:44 +0600

    service section intialized

--- components/Section/HomeSection/Services/Services.jsx
+++ components/Section/HomeSection/Services/Services.jsx
@@ -3,6 +3,7 @@ const Services = () => {
     return (
         <div className="min-h-screen mx-auto container flex items-center justify-center">
             <p className="text-5xl font-bold">All Services Here </p>
+            <h4>This section is for Tonmoy</h4>
         </div>
     );
 };
