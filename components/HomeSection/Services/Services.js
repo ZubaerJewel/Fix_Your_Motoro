d2aa9be9993c8725e8d940f55602c580e6705b5a
ZubaerJewel <zubaer5jewel.16@gmail.com>
2023-08-07 13:26:28 +0600

    add a  news

--- components/HomeSection/Services/Services.js
+++ components/HomeSection/Services/Services.js
@@ -2,7 +2,7 @@
 const Services = () => {
     return (
         <div className="min-h-screen mx-auto container flex items-center justify-center">
-            <p className="text-5xl font-bold">All Services Here</p>
+            <p className="text-5xl font-bold">All Services Here </p>
         </div>
     );
 };
