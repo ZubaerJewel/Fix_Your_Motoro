0a48e19cd0bab44dd61662b7a85d7df221a3c04d
Sabbir Sarker <masabbir1257@gmail.com>
2023-08-07 20:01:02 +0600

    Our Expert section add

--- app/page.js
+++ app/page.js
@@ -2,11 +2,14 @@ import { Hero } from "@/components/Section/HomeSection/Hero/Hero";
 import NewsTips from "@/components/Section/HomeSection/News/NewsTips";
 import Review from "@/components/Section/HomeSection/Review/Review";
 import Services from "@/components/Section/HomeSection/Services/Services";
+import OurExpert from "@/components/Section/HomeSection/OurExpert/OurExpert";
+
 
 const HomePage = () => {
   return (
     <main>
       <Hero />
+      <OurExpert/>
       <NewsTips />
       <Services />
       <Review />
---
+++ components/Section/HomeSection/OurExpert/OurExpert.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OurExpert = () => {
+    return (
+        <div>
+            <h1 className='py-8 text-5xl text-center primary-text'>Our Expert </h1>
+        </div>
+    );
+};
+
+export default OurExpert;
