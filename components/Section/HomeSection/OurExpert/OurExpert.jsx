b09f0219bd923fa33efdfaf1df5b4ad4688bf677
Sabbir Sarker <masabbir1257@gmail.com>
2023-08-07 20:10:39 +0600

    Our Expert section update

--- components/Section/HomeSection/OurExpert/OurExpert.jsx
+++ components/Section/HomeSection/OurExpert/OurExpert.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const OurExpert = () => {
     return (
         <div>
-            <h1 className='py-8 text-5xl text-center primary-text'>Our Expert </h1>
+            <h1 className='py-8 text-5xl text-center underline primary-text'>Our Expert </h1>
         </div>
     );
 };
