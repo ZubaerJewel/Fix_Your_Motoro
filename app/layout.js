d152024796ab31a53131dbf634ac016c4fd436e9
SM Sohag <sohagsheik32@gmail.com>
2023-08-07 18:57:11 +0600

    file stractcher change

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import Header from '@/components/Header/Header'
+import Header from '@/components/Shared/Header/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Footer from '@/components/Footer/Footer'
+import Footer from '@/components/Shared/Footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
